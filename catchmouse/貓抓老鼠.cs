55892baa312777c5c43bae291f28931cbc5b198f
unknown <012614@lacquercraft.com>
2023-11-08 17:02:41 +0800

    成功!

---
+++ catchmouse/貓抓老鼠.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace catchmouse
+{
+    public partial class 貓抓老鼠 : Form
+    {
+        public 貓抓老鼠()
+        {
+            InitializeComponent();
+            this.Load+=new EventHandler(Form2_Load);
+            this.BackColor = Color.LightSkyBlue;
+            
+            
+        }
+        protected void Form2_Load(object sender,EventArgs e)
+        {
+            Button newButton = new Button();
+            newButton.Text = "Select";// 設定按鈕上顯示的文字
+            newButton.Location = new Point(50, 200);// 設定按鈕在視窗中的位置
+            newButton.Size = new Size(50, 50);
+            newButton.BackColor = Color.Red;
+            this.Controls.Add(newButton);// 將按鈕新增到 Form 中
+            newButton.Click += new EventHandler(button_Click);
+            PictureBox welcome = new PictureBox();
+            welcome.Image = Image.FromFile("C:\\Users\\012614\\source\\repos\\catchmouse\\catchmouse\\image\\歡迎光臨.png");
+            welcome.SizeMode = PictureBoxSizeMode.StretchImage;// 将图像缩放以适应 PictureBox 大小
+            welcome.Size = new Size(350,350);
+            welcome.Location = new Point(210, 50);
+            this.Controls.Add(welcome);
+        }
+        private void button_Click(object sender,EventArgs e)
+        {
+            Form messageForm = new Form();
+            messageForm.Size = new Size(300,200);
+            messageForm.StartPosition = FormStartPosition.CenterParent;//显示该窗体时，它会相对于父窗体的中心位置进行定位
+
+            Label label = new Label();
+            label.Text = "請選擇挑戰難度:";
+            label.Location = new Point(10,10);
+            messageForm.Controls.Add(label);
+
+            Button button1 = new Button();
+            button1.Text = "速度10";
+            button1.Location = new Point(110,60);
+            button1.Size = new Size(100,30);
+            button1.Click += (s, args) => messageForm.Close();
+            button1.Click += new EventHandler(button1_Click);
+            messageForm.Controls.Add(button1);
+            Button button2 = new Button();
+            button2.Text = "速度100";
+            button2.Location = new Point(110, 95);
+            button2.Size = new Size(100, 30);
+            button2.Click += (s, args) => messageForm.Close();
+            button2.Click += new EventHandler(button2_Click);
+            messageForm.Controls.Add(button2);
+            messageForm.ShowDialog();
+            
+
+        }
+        private void button1_Click(object sender,EventArgs e)
+        {
+            // 关闭当前的 Form2
+            this.Hide();
+            // 新增現有的Form1實例
+            速度10 form1 = new 速度10();
+
+            //关闭 Form2 当 Form1 关闭时
+            form1.FormClosed += (s, args) => this.Close();
+            form1.Show();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 关闭当前的 Form2
+            this.Hide();
+            // 新增現有的Form1實例
+            速度100 form3 = new 速度100();
+
+            //关闭 Form2 当 Form1 关闭时
+            form3.FormClosed += (s, args) => this.Close();
+            form3.Show();
+        }
+    }
+}
---
+++ catchmouse/速度10.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace catchmouse
+{
+    public partial class 速度10 : Form
+    {
+        private PictureBox cat;
+        private PictureBox mouse;
+        private System.Windows.Forms.Timer timerMouseMovement;//是 Windows 窗体应用程序中用于处理定时器功能的类
+        // 假设mouseSpeedX和mouseSpeedY分别是老鼠在X和Y方向上的移动速度(初始速度)
+        Random random = new Random();
+        int mouseSpeedX = 10;
+        int mouseSpeedY = 10;
+        
+        public 速度10()
+        {
+            InitializeComponent();
+            InitializeGame();
+            this.Load += new EventHandler(Form1_Load);// 将Form1_Load方法与窗体的Load事件相关联
+            //初始化timerMouseMovement對象
+            timerMouseMovement = new System.Windows.Forms.Timer();
+            timerMouseMovement.Interval = 1000;// 设置定时器间隔，单位为毫秒，1000 毫秒（即 1 秒）触发一次 Tick 事件
+            timerMouseMovement.Tick += new EventHandler(timerMouseMovement_Tick);// 将定时器事件处理程序关联到事件
+            timerMouseMovement.Start();// 启动定时器
+        }
+        private void InitializeGame()
+        {
+            // 创建猫
+            cat = new PictureBox();
+            cat.Image = Image.FromFile("C:\\Users\\012614\\source\\repos\\catchmouse\\catchmouse\\image\\cat.jpg"); // 请确保图片存在于相应位置
+            cat.SizeMode = PictureBoxSizeMode.StretchImage;
+            cat.Size = new Size(50, 50);
+            cat.Location = new Point(50, 50);
+            this.Controls.Add(cat);
+
+            // 创建老鼠
+            mouse = new PictureBox();
+            mouse.Image = Image.FromFile("C:/Users/012614/source/repos/catchmouse/catchmouse/image/mouse.jpg"); // 请确保图片存在于相应位置
+            mouse.SizeMode = PictureBoxSizeMode.StretchImage;//设置了 PictureBox 控件中图像的显示方式为拉伸（StretchImage），Zoom為壓縮
+            mouse.Size = new Size(30, 30);
+
+            //生成隨機位置
+            
+            int maxX = this.ClientSize.Width - mouse.Width;
+            int maxY = this.ClientSize.Height - mouse.Height;
+            int randomX = random.Next(0, maxX);
+            int randomY = random.Next(0, maxY); 
+
+            //設置老鼠隨機位置
+            mouse.Location = new Point(randomX, randomY);
+            this.Controls.Add(mouse);
+
+
+            // 添加键盘事件
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.Focus();//用户可以直接与窗体交互，而不需要先点击窗体才能开始使用键盘
+        }
+        private void Form1_Load(object sender,EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile("C:\\Users\\012614\\source\\repos\\catchmouse\\catchmouse\\image\\背景.jpg");
+            this.BackgroundImageLayout = ImageLayout.Stretch;// 设置图片布局，可根据需要调整
+            
+        }
+        private void timerMouseMovement_Tick(object sender, EventArgs e)
+        {
+            //以一定概率改變速度方向
+            if(random.Next(100)<20) //例如、20%概率改變方向
+            {
+                mouseSpeedX = random.Next(-5, 6);//隨機X方向速度
+                mouseSpeedY = random.Next(-5, 6);//隨機Y方向速度
+            }
+            
+            // 更新老鼠的位置
+            mouse.Left += mouseSpeedX;
+            mouse.Top += mouseSpeedY;
+            if(mouse.Left<=0||mouse.Right>=this.ClientSize.Width)
+            {
+                mouseSpeedX = -mouseSpeedX;
+            }
+            if(mouse.Top<=0||mouse.Bottom>=this.ClientSize.Height)
+            {
+                mouseSpeedY = -mouseSpeedY;
+            }
+            if(cat.Bounds.IntersectsWith(mouse.Bounds))
+            {
+                timerMouseMovement.Stop();
+            }
+        }   
+        private void Form1_KeyDown(object sender, KeyEventArgs e)//KeyDown 事件处理程序中，可以检查 e.KeyCode 的值来确定用户按下了哪个键，并相应地执行相应的操作。
+        {
+            // 移动猫
+            int step = 10;
+            if (e.KeyCode == Keys.Left)
+            {
+                cat.Left -= step;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                cat.Left += step;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                cat.Top -= step;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                cat.Top += step;
+            }
+
+            // 检查猫和老鼠的碰撞
+            if (cat.Bounds.IntersectsWith(mouse.Bounds))
+            {
+                MessageBox.Show("猫抓到了老鼠！");
+            }
+        }
+
+    }
+}
